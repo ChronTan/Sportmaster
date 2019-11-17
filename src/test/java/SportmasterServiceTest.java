c977a8a3e96862baa6f51ed736b57155747a5000
NoName <skarr.age@mail.ru>
2019-11-17 12:18:56 +0300

    update tests covarage

--- src/test/java/SportmasterServiceTest.java
+++ src/test/java/SportmasterServiceTest.java
@@ -10,6 +10,10 @@ class SportmasterServiceTest {
         float cost=smService.getCostOfAllPurchases();
         smService.levelCard(cost);
         assertEquals("Gold",smService.getLevelCard());
+        smService.levelCard(10_000);
+        assertEquals("Standart",smService.getLevelCard());
+        smService.levelCard(100_000);
+        assertEquals("Silver",smService.getLevelCard());
 
     }
 
@@ -19,6 +23,12 @@ class SportmasterServiceTest {
         smService.levelCard(smService.getCostOfAllPurchases());
         float result=smService.accounting(smService);
         assertEquals(50,result);
+        smService.levelCard(10_000_000);
+        result=smService.accounting(smService);
+        assertEquals(100,result);
+        smService.levelCard(100_000);
+        result=smService.accounting(smService);
+        assertEquals(70,result);
     }
 
 
