d009d90cddacdd73470e2409ecff393767985811
NoName <skarr.age@mail.ru>
2019-11-19 10:54:10 +0300

    fix code

--- src/main/java/Sportmaster.java
+++ src/main/java/Sportmaster.java
@@ -31,33 +31,5 @@ public class Sportmaster {
         this.costPurchases = costPurchases;
     }
 
-    //    public void levelCard(int cost){
-//        if(cost<=15000){
-//            levelCard="Standart";
-//        }else if(cost<=150000){
-//            levelCard="Silver";
-//        }else{
-//            levelCard="Gold";
-//        }
-//    }
-//
-//    public int accounting(Sportmaster sms){
-//        int bonus=0;
-//        switch (sms.levelCard){
-//            case "Standart":
-//                bonus=(int)Math.floor(costPurchases/1000)*50;
-//                break;
-//            case "Silver":
-//                bonus=(int)Math.floor(costPurchases/1000)*70;
-//                break;
-//            case "Gold":
-//                bonus=(int)Math.floor(costPurchases/1000)*100;
-//                break;
-//                default:
-//                    System.out.println("Такой категории нет");
-//                    break;
-//        }
-//
-//        return bonus;
-//    }
+
 }
